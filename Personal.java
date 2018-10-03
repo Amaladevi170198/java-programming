efa3d810645f70542e099ae3dc55e62b4692874a
Amaladevi170198 <43032319+Amaladevi170198@users.noreply.github.com>
2018-10-03 12:14:57 +0530

    Create Personal.java

---
+++ Personal.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Personal
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{ 
+      String name = "Amaladevi";
+        String nickname = "amala";
+        String city = "nagercoil";
+        int age = 20;
+        float height = 5.3f;
+        boolean married =false;
+        char sex = 'F';
+        double population = 4828853;
+        System.out.println("\n name:" +name+ "\n city:" +city+ "\n age:" +age+ "\n height:" +height+ " \n married:" +married+ " \n sex:" +sex+ " \n population:"+population);
+	}
+}

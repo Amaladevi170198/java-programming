5383709bd1534f2352deb7bd2c74a137a61d36c4
Amaladevi170198 <43032319+Amaladevi170198@users.noreply.github.com>
2018-10-05 12:37:53 +0530

    Update Vowel.java

--- Vowel.java
+++ Vowel.java
@@ -2,7 +2,7 @@ class vowel{
 
     public static void main(String[] args) {
 
-        char ch = 'u';
+        char ch = 'e';
 
         if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
             System.out.println("vowel");
